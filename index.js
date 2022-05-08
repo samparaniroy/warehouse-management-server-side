d2af9c942ce7941b55aeede913165e85f7905522
Sampa Rani Roy <samparaniroy99@gmail.com>
2022-05-08 23:13:56 +0600

    adding steps

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,14 +16,35 @@ async function run(){
     try{
         await client.connect();
         const productCollection = client.db('warehouse').collection('product');
+        
+        // Auth
+        app.get('login', async(req, res) =>{
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+                expiresIn: '1d'
+            })
+            res.send({accessToken})
+        })
+        // update api
+        // app.put('product/:id', async(req, res) =>{
+        //     const id = req.params.id;
+        //     const total = req.body;
+        //     const filter = {_id: ObjectId(id)}
+        //     const options ={upsert: true};
+        //     const updatedDoc = {
+        //         $set: {
+        //             quantity: total.quantity
+        //         }
+        //     }
+        //     const result = await productCollection.updateOne(filter, options, updatedDoc);
+        //     res.send(result)
+        // })
         app.get('/product', async (req, res)=>{
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products)
         });
-        
-        //Get order api
         app.get('/myitems',async(req,res) =>{
             const email = req.query.email;
             const query = {email:email}
